b3160b798a4c803a5084f31db207693908e15b7a
Alex <am3081@gmail.com>
2013-10-25 15:22:35 -0400

    new stuff

--- data/create/createCountyTable.php
+++ data/create/createCountyTable.php
@@ -1,31 +1,42 @@
 <?php
-$county_fips = $_POST['county_fips'];
-$state_fips = $_POST['state_fips']; 
-
+error_reporting(E_ALL ^ E_DEPRECATED);
 include '../config/db.php'; 
 $test = new db();
 
 $inscon = $test->connect();
-$sql = "CREATE TABLE `MN_$fips` (
-`mode` varchar(45) DEFAULT NULL,
-`orig_fips` varchar(45) DEFAULT NULL,
-`dest_fips` varchar(45) DEFAULT NULL,
-`sctg2` varchar(45) DEFAULT NULL,
-`all_tons` float DEFAULT NULL,
-`orig_state` varchar(45) DEFAULT NULL,
-`orig_county` varchar(45) DEFAULT NULL,
-`dest_state` varchar(45) DEFAULT NULL,
-`dest_county` varchar(45) DEFAULT NULL,
-KEY `orig` (`orig_fips`),
-KEY `dest` (`dest_fips`),
-KEY `orig-state` (`orig_state`),
-KEY `dest-state` (`dest_state`),
-KEY `mode` (`mode`),
-KEY `scgt` (`sctg2`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1";
-mysql_query($sql) or die($sql." ".mysql_error());
-$sql ="insert into `".$state_fips."_".$fips."` select * from MN_All_Flows where orig_fips = $fips or dest_fips = $fips";
-mysql_query($sql) or die($sql." ".mysql_error());
+
+$sql = "select distinct dms_orig_fips from tbl_maine_domestic_db_3 where dms_orig_fips like '55___' ";
+$rs = mysql_query($sql) or die($sql." ".mysql_error());
+
+while($row = mysql_fetch_array($rs)){
+	$state_fips = substr($row['dms_orig_fips'],0,2);
+	$county_fips = substr($row['dms_orig_fips'],2,3);
+	$fips = $row['dms_orig_fips'];
+	echo $state_fips."_".$fips."<br>";
+
+
+	$sql = "CREATE TABLE `".$state_fips.$county_fips."` (
+	  `dms_orig` varchar(45) DEFAULT NULL,
+	  `dms_orig_fips` varchar(45) DEFAULT NULL,
+	  `dms_dest` varchar(45) DEFAULT NULL,
+	  `dms_dest_fips` varchar(45) DEFAULT NULL,
+	   `dms_mode` varchar(45) DEFAULT NULL,
+	  `sctg2` varchar(45) DEFAULT NULL,
+	  `value_2010` float DEFAULT NULL,
+	  `curval_2010` float DEFAULT NULL,
+	  `tons_2010` float DEFAULT NULL,
+	  KEY `orig` (`dms_orig_fips`),
+	  KEY `dest` (`dms_dest_fips`),
+	  KEY `orig-dest` (`dms_dest_fips`,`dms_orig_fips`),
+	  KEY `mode` (`dms_mode`),
+	  KEY `scgt` (`sctg2`)
+	) ENGINE=InnoDB DEFAULT CHARSET=utf8";
+	mysql_query($sql) or die($sql." ".mysql_error());
+	$sql ="insert into `".$state_fips.$county_fips."` select * from tbl_maine_domestic_db_3 where dms_orig_fips = $fips or dms_dest_fips = $fips";
+	mysql_query($sql) or die($sql." ".mysql_error());
+	echo json_encode("table ".$state_fips.$fips." created.");
+}
+
 	
-echo json_encode("table ".$state_fips."_".$fips." created.");
+echo json_encode("Finished");
 ?>
---
+++ data/get/getCountyToNation_ME.php
@@ -0,0 +1,58 @@
+<?php
+	header('Access-Control-Allow-Origin: *'); 
+	error_reporting(E_ALL ^ E_DEPRECATED);
+	$commodity =$_POST['sctg'];
+	$mode = $_POST['mode'];
+	$fips = $_POST['fips'];
+	$orig_or_dest = $_POST['orig_or_dest'];
+	$orig_or_dest = "dms_".$orig_or_dest;
+	
+	$opposite = 'dms_dest_fips';
+
+	if($orig_or_dest == 'orig_fips'){
+		$opposite = 'dms_dest_fips';
+	}else{
+		$opposite = 'dms_orig_fips';
+	}
+
+	$comm_clause = '';
+	if($commodity != '00')
+	{
+		$comm_clause = " and sctg2 = '$commodity' ";
+	}
+
+	$mode_clause = '';
+	if($mode != '00')
+	{
+		$mode_clause = " and dms_mode = '$mode' ";
+	}
+
+	include '../config/db.php'; 
+	$test = new db();
+	$inscon = $test->connect();
+	$sql = "select $opposite,sum(tons_2010) as all_tons from `$fips` where $orig_or_dest = '$fips' $comm_clause $mode_clause group by $opposite order by all_tons desc ";
+	$csv = array();
+	$i = 0;
+	$rs=mysql_query($sql) or die($sql." ".mysql_error());
+	while($row = mysql_fetch_assoc( $rs )){
+
+		$csvrow['orig'] = $row[$opposite];
+		$csvrow['tons'] = $row['all_tons'];
+		$csv[] = $csvrow;
+
+	}
+	$flow = array();
+	$sql = "select * from counties_orig_fips where orig_fips = $fips";
+	$rs=mysql_query($sql) or die($sql." ".mysql_error());
+	$row = mysql_fetch_assoc( $rs );
+	$flow['orig_fips'] = $row;
+	$sql = "select * from counties_dest_fips where orig_fips = $fips";
+	$rs=mysql_query($sql) or die($sql." ".mysql_error());
+	$row = mysql_fetch_assoc( $rs );
+	$flow['dest_fips'] = $row;
+	
+	$output = array();
+	$output['flow'] = $flow;
+	$output['map'] = $csv; 
+	echo json_encode($output);
+?>
